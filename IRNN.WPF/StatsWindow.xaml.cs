b709cd7d4f737ab6d9565e3dcd89169f95d4bd87
Simone Lugaresi <simone.lugaresi@local.ittcesena.it>
2019-04-13 12:55:24 +0200

    start working on SparrowChart figuring out how it works.

--- IRNN.WPF/StatsWindow.xaml.cs
+++ IRNN.WPF/StatsWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sparrow.Chart;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace IRNN.WPF
 {
@@ -21,6 +23,7 @@ namespace IRNN.WPF
     {
         private App _main;
         private NeuralNetwork _network;
+        private int NumEpoche;
         public StatsWindow()
         {
             InitializeComponent();
@@ -28,14 +31,54 @@ namespace IRNN.WPF
 
         private void Window_Initialized(object sender, EventArgs e) {
             _main = Application.Current as App;
+            //TODO prendere dal file (di dime) settings.cfg il numero delle epoche(line 8) salvarno in NumEpoche
+
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             _network = _main.Network;
+            caricaGrafico();
             caricaLista(lst_layers, _network.NeuralLayers);
         }
 
+        private void caricaGrafico()
+        {
+            //prova con i dati a caso
+            spl_error.Points.Add(new DoublePoint() { Data = 10, Value = 2 });
+            spl_error.Points.Add(new DoublePoint() { Data = 450, Value = 5 });
+            //TODO passano un file con lerrore globale per i neuroni di ogni epoca 
+            //tipo file txt separato da virgole  leggere 1 epoca (x) e l'errore (y)
+
+            string path = "";
+            string[,] DatStringError=LeggiFile(); 
+        }
+
+        private string[,] LeggiFile()
+        {
+            string path="";
+            List<string[]> risultati = new List<string[]>();//lista per aggiungere ogni volta un vettore con ADD
+            //TODO fai una lista di lista per ogni volta aggiungere i punti
+            if (File.Exists(path))
+            {
+                StreamReader sr = File.OpenText(path);
+                //lettura righe
+                string testo = "";
+                for (int i = 0; testo != null; i++)
+                {
+                    testo = sr.ReadLine();
+                    string[] testo1 = testo.Split(',');
+                    risultati.Add(testo);
+                }
+                sr.Close();
+            }
+            else
+            {
+                MessageBox.Show("Error interno mancata creazione file degli errori globali", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+        
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
             if (_main.HasClosed)
                 return;
@@ -61,6 +104,7 @@ namespace IRNN.WPF
 
             var neuron = _network.NeuralLayers[lst_layers.SelectedIndex].Neurons[lst_neurons.SelectedIndex];
             //TODO gestire la stampa del neurone con il toString
+            
         }
 
         private void lst_layers_SelectionChanged(object sender, SelectionChangedEventArgs e)
