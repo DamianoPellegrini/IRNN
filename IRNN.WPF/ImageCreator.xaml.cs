ee165f4e80e98eb8f63bcf11585d607d6d8371d5
Damiano Pellegrini <damiano1.pellegrini@gmail.com>
2019-04-14 15:24:47 +0200

    Bitmap encoding and saving.

--- IRNN.WPF/ImageCreator.xaml.cs
+++ IRNN.WPF/ImageCreator.xaml.cs
@@ -1,28 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using IRNN.WPF.Utils;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using IRNN.WPF.Utils;
 
-namespace IRNN.WPF
-{
+namespace IRNN.WPF {
+
     /// <summary>
     /// Logica di interazione per ImageCreator.xaml
     /// </summary>
-    public partial class ImageCreator : Window
-    {
+    public partial class ImageCreator : Window {
         private App _main;
-        public ImageCreator()
-        {
+
+        public ImageCreator() {
             InitializeComponent();
         }
 
@@ -38,9 +27,45 @@ namespace IRNN.WPF
             _main.MenuWindow.Show();
         }
 
-        private void menu_salva(object sender, RoutedEventArgs e)
-        {
-            InkCanvasToPBMConverter.InkCanvasToBitmap(grd_inkWrapper);
+        private void menu_salva(object sender, RoutedEventArgs e) {
+            //TODO: SAVE FILE DIALOG CON PATH
+            InkCanvasToPBMConverter.InkCanvasToBitmap(ink_drawingBoard);
+        }
+
+        private void menu_pulisci(object sender, RoutedEventArgs e) {
+            ink_drawingBoard.Strokes.Clear();
+        }
+
+        private void menu_esci(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+
+        private void switchTool(object sender, SelectionChangedEventArgs e) {
+            if (chb_tool.SelectedItem == chb_tool.Items[0])
+                ink_drawingBoard.EditingMode = InkCanvasEditingMode.Ink;
+            if (chb_tool.SelectedItem == chb_tool.Items[1])
+                ink_drawingBoard.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+
+        private void switchSize(object sender, SelectionChangedEventArgs e) {
+            //TODO: Se il programma funziona scoprire perchè non va
+            //ComboBoxItem item = sender as ComboBoxItem;
+            //if (item == null)
+            //    return;
+
+            //double size = double.Parse(item.Content.ToString());
+            //ink_drawingBoard.DefaultDrawingAttributes = new System.Windows.Ink.DrawingAttributes() {
+            //    Width = size,
+            //    Height = size,
+            //    Color = System.Windows.Media.Color.FromRgb(0,0,0),
+            //    FitToCurve = false,
+            //    IgnorePressure = false,
+            //    IsHighlighter = false,
+            //    StylusTip = System.Windows.Ink.StylusTip.Ellipse,
+            //    StylusTipTransform = System.Windows.Media.Matrix.Identity
+            //};
+            //ink_drawingBoard.UpdateDefaultStyle();
+            //ink_drawingBoard.UpdateLayout();
         }
     }
 }
--- IRNN.WPF/Utils/InkCanvasToPBMConverter.cs
+++ IRNN.WPF/Utils/InkCanvasToPBMConverter.cs
@@ -4,40 +4,43 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+
 
 namespace IRNN.WPF.Utils
 {
     internal class InkCanvasToPBMConverter
     {
-        internal static BitmapImage InkCanvasToBitmap(Grid canvasWrapper)
+        internal static BitmapImage InkCanvasToBitmap(InkCanvas canvas)
         {
-            int width = (int)canvasWrapper.ActualWidth;
-            int height = (int)canvasWrapper.ActualHeight;
-            RenderTargetBitmap renderBitmap = new RenderTargetBitmap(width, height, 300d, 300d, PixelFormats.Default);
-            renderBitmap.Render(canvasWrapper);
+            //RENDERING
+            int width = (int)canvas.ActualWidth;
+            int height = (int)canvas.ActualHeight;
+            RenderTargetBitmap renderBitmap = new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Default);
+            renderBitmap.Render(canvas);
+
+            //ENCODING
             BitmapEncoder encoder = new BmpBitmapEncoder();
             encoder.Frames.Add(BitmapFrame.Create(renderBitmap));
 
-            //MemoryStream stream = new MemoryStream();
-            //encoder.Save(stream);
-#if DEBUG
-            FileStream fs = File.Create("test.bmp");
-            encoder.Save(fs);
-            fs.Close();
-#endif
-
+            //SAVING TO BITMAP OBJECT
+            MemoryStream stream = new MemoryStream();
+            encoder.Save(stream);
             BitmapImage ret = new BitmapImage();
-            //ret.StreamSource = stream;
-
-            //stream.Close();
-
+            ret.StreamSource = stream;
+            stream.Close();
             return ret;
         }
+
         internal static Color[,] BitmapToColorMatrix(BitmapImage image)
         {
+            throw new NotImplementedException();
+        }
+
+        internal static object/*TODO: Substitute to PBMImage on merge*/ ColorMatrixToPBMImage(Color[,] image) {
             throw new NotImplementedException();
         }
     }
